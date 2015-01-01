e62fcadb8e0d9235d78b2c83f36fbe181840b1ef
Jeff Prestes <jeffprestes@gmail.com>
2015-01-01 12:26:23 -0200

    Funcao de autocompletar para nome dos alimentos

--- pontos-insere.php
+++ pontos-insere.php
@@ -46,7 +46,9 @@ if ($_GET["cod_tprefeicao"]==null)   {
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1" /> 
     <title>Tabela Elimina Peso - Pontos - Inclus&atilde;o</title>
-    <link rel="stylesheet" href="http://vg.novatrix.com.br/jquery/themes/base/jquery.ui.all.css"></link>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
     <style type="text/css">
         <!--
         body,td,th {
@@ -81,9 +83,156 @@ if ($_GET["cod_tprefeicao"]==null)   {
 		margin: 0;
 		padding: 0.3em;
 	}
+        .custom-combobox {
+		position: relative;
+		display: inline-block;
+	}
+	.custom-combobox-toggle {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		margin-left: -1px;
+		padding: 0;
+	}
+	.custom-combobox-input {
+		margin: 0;
+		padding: 5px 10px;
+	}
         -->
     </style>
     <link href="estilos.css" rel="stylesheet" type="text/css" />
+    <script>
+	(function( $ ) {
+		$.widget( "custom.combobox", {
+			_create: function() {
+				this.wrapper = $( "<span>" )
+					.addClass( "custom-combobox" )
+					.insertAfter( this.element );
+
+				this.element.hide();
+				this._createAutocomplete();
+				this._createShowAllButton();
+			},
+
+			_createAutocomplete: function() {
+				var selected = this.element.children( ":selected" ),
+					value = selected.val() ? selected.text() : "";
+
+				this.input = $( "<input>" )
+					.appendTo( this.wrapper )
+					.val( value )
+					.attr( "title", "" )
+					.addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+					.autocomplete({
+						delay: 0,
+						minLength: 0,
+						source: $.proxy( this, "_source" )
+					})
+					.tooltip({
+						tooltipClass: "ui-state-highlight"
+					});
+
+				this._on( this.input, {
+					autocompleteselect: function( event, ui ) {
+						ui.item.option.selected = true;
+						this._trigger( "select", event, {
+							item: ui.item.option
+						});
+					},
+
+					autocompletechange: "_removeIfInvalid"
+				});
+			},
+
+			_createShowAllButton: function() {
+				var input = this.input,
+					wasOpen = false;
+
+				$( "<a>" )
+					.attr( "tabIndex", -1 )
+					.attr( "title", "Show All Items" )
+					.tooltip()
+					.appendTo( this.wrapper )
+					.button({
+						icons: {
+							primary: "ui-icon-triangle-1-s"
+						},
+						text: false
+					})
+					.removeClass( "ui-corner-all" )
+					.addClass( "custom-combobox-toggle ui-corner-right" )
+					.mousedown(function() {
+						wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+					})
+					.click(function() {
+						input.focus();
+
+						// Close if already visible
+						if ( wasOpen ) {
+							return;
+						}
+
+						// Pass empty string as value to search for, displaying all results
+						input.autocomplete( "search", "" );
+					});
+			},
+
+			_source: function( request, response ) {
+				var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+				response( this.element.children( "option" ).map(function() {
+					var text = $( this ).text();
+					if ( this.value && ( !request.term || matcher.test(text) ) )
+						return {
+							label: text,
+							value: text,
+							option: this
+						};
+				}) );
+			},
+
+			_removeIfInvalid: function( event, ui ) {
+
+				// Selected an item, nothing to do
+				if ( ui.item ) {
+					return;
+				}
+
+				// Search for a match (case-insensitive)
+				var value = this.input.val(),
+					valueLowerCase = value.toLowerCase(),
+					valid = false;
+				this.element.children( "option" ).each(function() {
+					if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+						this.selected = valid = true;
+						return false;
+					}
+				});
+
+				// Found a match, nothing to do
+				if ( valid ) {
+					return;
+				}
+
+				// Remove invalid value
+				this.input
+					.val( "" )
+					.attr( "title", value + " didn't match any item" )
+					.tooltip( "open" );
+				this.element.val( "" );
+				this._delay(function() {
+					this.input.tooltip( "close" ).attr( "title", "" );
+				}, 2500 );
+				this.input.autocomplete( "instance" ).term = "";
+			},
+
+			_destroy: function() {
+				this.wrapper.remove();
+				this.element.show();
+			}
+		});
+	})( jQuery );
+
+	</script>
 </head>
 <body>
     <!-- <div data-role="page" class="type-interior"> -->
@@ -137,11 +286,14 @@ if ($_GET["cod_tprefeicao"]==null)   {
                   <tr>
                 <th scope="row"><div align="right" class="labelCampo">Ref.:</div></th>
                 <td align="left">
+                    <div class="ui-widget">
                     <? $sql = "SELECT cod_tprefeicao as objCodigo, desc_tprefeicao as objDescricao FROM tipo_refeicao ORDER BY ordem";
                            $res = mysql_query($sql, $link);
                            montaComboComFuncaoComTabIndex($res, "cod_tprefeicao", $tprefeicao, "", "4");
                            mysql_free_result($res);
-                         ?>        </td>
+                         ?>
+                    </div>
+                </td>
               </tr>
               <tr>
                 <th scope="row"><div align="right" class="labelCampo">Alim:</div></th>
@@ -248,14 +400,6 @@ if ($_GET["cod_tprefeicao"]==null)   {
             </table></th>
           </tr>
         </table>
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.core.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.widget.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.button.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.position.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.menu.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.autocomplete.js"></script>
-	<script src="http://view.jqueryui.com/master/ui/jquery.ui.tooltip.js"></script>
         <script language="javascript">
                 function novoAlimento()	{
                         document.location='alimentos-insere.php?destino=pontos-insere.php';
@@ -312,140 +456,12 @@ if ($_GET["cod_tprefeicao"]==null)   {
                 }
                 
                 
-                //codigo para autocomplete
-                (function( $ ) {
-                $.widget( "ui.combobox", {
-                        _create: function() {
-                                var input,
-                                        that = this,
-                                        select = this.element.hide(),
-                                        selected = select.children( ":selected" ),
-                                        value = selected.val() ? selected.text() : "",
-                                        wrapper = $( "<span>" )
-                                                .addClass( "ui-combobox" )
-                                                .insertAfter( select );
-
-                                function removeIfInvalid(element) {
-                                        var value = $( element ).val(),
-                                                matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( value ) + "$", "i" ),
-                                                valid = false;
-                                        select.children( "option" ).each(function() {
-                                                if ( $( this ).text().match( matcher ) ) {
-                                                        this.selected = valid = true;
-                                                        return false;
-                                                }
-                                        });
-                                        if ( !valid ) {
-                                                // remove invalid value, as it didn't match anything
-                                                $( element )
-                                                        .val( "" )
-                                                        .attr( "title", value + " didn't match any item" )
-                                                        .tooltip( "open" );
-                                                select.val( "" );
-                                                setTimeout(function() {
-                                                        input.tooltip( "close" ).attr( "title", "" );
-                                                }, 2500 );
-                                                input.data( "autocomplete" ).term = "";
-                                                return false;
-                                        }
-                                }
-
-                                input = $( "<input>" )
-                                        .appendTo( wrapper )
-                                        .val( value )
-                                        .attr( "title", "" )
-                                        .addClass( "ui-state-default" )
-                                        .autocomplete({
-                                                delay: 0,
-                                                minLength: 0,
-                                                source: function( request, response ) {
-                                                        var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-                                                        response( select.children( "option" ).map(function() {
-                                                                var text = $( this ).text();
-                                                                if ( this.value && ( !request.term || matcher.test(text) ) )
-                                                                        return {
-                                                                                label: text.replace(
-                                                                                        new RegExp(
-                                                                                                "(?![^&;]+;)(?!<[^<>]*)(" +
-                                                                                                $.ui.autocomplete.escapeRegex(request.term) +
-                                                                                                ")(?![^<>]*>)(?![^&;]+;)", "gi"
-                                                                                        ), "<strong>$1</strong>" ),
-                                                                                value: text,
-                                                                                option: this
-                                                                        };
-                                                        }) );
-                                                },
-                                                select: function( event, ui ) {
-                                                        ui.item.option.selected = true;
-                                                        that._trigger( "selected", event, {
-                                                                item: ui.item.option
-                                                        });
-                                                },
-                                                change: function( event, ui ) {
-                                                        if ( !ui.item )
-                                                                return removeIfInvalid( this );
-                                                }
-                                        })
-                                        .addClass( "ui-widget ui-widget-content ui-corner-left" );
-
-                                input.data( "autocomplete" )._renderItem = function( ul, item ) {
-                                        return $( "<li>" )
-                                                .data( "item.autocomplete", item )
-                                                .append( "<a>" + item.label + "</a>" )
-                                                .appendTo( ul );
-                                };
-
-                                $( "<a>" )
-                                        .attr( "tabIndex", -1 )
-                                        .attr( "title", "Show All Items" )
-                                        .tooltip()
-                                        .appendTo( wrapper )
-                                        .button({
-                                                icons: {
-                                                        primary: "ui-icon-triangle-1-s"
-                                                },
-                                                text: false
-                                        })
-                                        .removeClass( "ui-corner-all" )
-                                        .addClass( "ui-corner-right ui-button-icon" )
-                                        .click(function() {
-                                                // close if already visible
-                                                if ( input.autocomplete( "widget" ).is( ":visible" ) ) {
-                                                        input.autocomplete( "close" );
-                                                        removeIfInvalid( input );
-                                                        return;
-                                                }
-
-                                                // work around a bug (likely same cause as #5265)
-                                                $( this ).blur();
-
-                                                // pass empty string as value to search for, displaying all results
-                                                input.autocomplete( "search", "" );
-                                                input.focus();
-                                        });
-
-                                        input
-                                                .tooltip({
-                                                        position: {
-                                                                of: this.button
-                                                        },
-                                                        tooltipClass: "ui-state-highlight"
-                                                });
-                        },
-
-                        destroy: function() {
-                                this.wrapper.remove();
-                                this.element.show();
-                                $.Widget.prototype.destroy.call( this );
-                        }
+                $(function() {
+                    $( "#cod_alimento" ).combobox();
+                    $( "#toggle" ).click(function() {
+			$( "#combobox" ).toggle();
+                    });
                 });
-        })( jQuery );
-
-        $(function() {
-                $( "#cod_alimento" ).combobox();
-                document.frm.cod_alimento.focus();
-                
-        });
         </script>
     <!-- </div> -->
 </body>
